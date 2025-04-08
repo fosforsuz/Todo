a9e6712261e7291da66aa289482fa2397d98c508
Talha Enes Baba <60211676+fosforsuz@users.noreply.github.com>
2025-04-08 21:16:04 +0300

    feat: Enhance application configuration with RabbitMQ integration and service registration

--- Todo.User.Application/ConfigureApplication.cs
+++ Todo.User.Application/ConfigureApplication.cs
@@ -1,8 +1,15 @@
+using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Todo.Shared.Contracts.Config;
+using Todo.SharedKernel.Messaging;
 using Todo.User.Application.Abstraction;
+using Todo.User.Application.Behaviour;
 using Todo.User.Application.Services;
 using Todo.User.Infrastructure;
+using Todo.User.Infrastructure.Abstraction;
+using Todo.User.Infrastructure.Messaging;
 
 namespace Todo.User.Application;
 
@@ -11,16 +18,31 @@ public static class ConfigureApplication
     public static void ConfigureModules(this IServiceCollection services, IConfiguration configuration)
     {
         var connectionString = GetConnectionString(configuration);
+
         services.AddInfrastructure(connectionString);
-        services.AddApplication();
+        services.AddApplication(configuration);
     }
 
-    private static void AddApplication(this IServiceCollection services)
+    private static void AddApplication(this IServiceCollection services, IConfiguration configuration)
     {
-        services.ConfigureServices();
+        // Bind RabbitMQ settings (IOptions<RabbitMqConfig>)
+        services.Configure<RabbitMqConfig>(configuration.GetSection(nameof(RabbitMqConfig)));
+
+        // Register application-specific services
+        services.AddDomainServices();
+        services.ConfigureRabbitMqPublishers();
+
+        // Register MediatR handlers
+        services.AddMediatR(cfg => { cfg.RegisterServicesFromAssembly(typeof(ConfigureApplication).Assembly); });
+
+        // Register FluentValidation validators
+        services.AddValidatorsFromAssembly(typeof(ConfigureApplication).Assembly);
+
+        // Register Validation pipeline for MediatR
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationPipeline<,>));
     }
 
-    private static void ConfigureServices(this IServiceCollection services)
+    private static void AddDomainServices(this IServiceCollection services)
     {
         services.AddScoped<IAuthService, AuthService>();
         services.AddScoped<ILoginHistoryService, LoginHistoryService>();
@@ -28,6 +50,11 @@ public static class ConfigureApplication
         services.AddScoped<IUserService, UserService>();
     }
 
+    private static void ConfigureRabbitMqPublishers(this IServiceCollection services)
+    {
+        services.AddSingleton<ILogEventPublisher, RabbitMqLogEventPublisher>();
+        services.AddSingleton<IRabbitMqEmailPublisher, RabbitMqEmailPublisher>();
+    }
 
     private static string GetConnectionString(IConfiguration configuration)
     {
